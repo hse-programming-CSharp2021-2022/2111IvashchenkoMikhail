453699dd1c88c6326cd9a669929aab346ac4d4ea
Martinez1337 <msivaschenko@edu.hse.ru>
2021-11-20 01:47:07 +0300

    task 5

---
+++ module_2/Seminar_5.11/Seminar_5.11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Seminar_5._11
+{
+    class RightTriangle
+    {
+        private int a;
+        public static int count = 0;
+        public static readonly int count2 = 0;
+        public const int count3 = 0;
+        static RightTriangle()
+        {
+            count = 10;
+            count2 = 100;
+            //count3 = 1000;
+        }
+        public int A { get; set; }
+
+        public RightTriangle(int a)
+        {
+            A = a;
+            count++;
+        }
+
+        ~RightTriangle()
+        {
+            Console.WriteLine("destructor");
+        }
+        
+        public RightTriangle() : this(0) { }
+
+        public RightTriangle(int a, int b)
+        {
+            new RightTriangle();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RightTriangle.count);
+            Console.WriteLine(RightTriangle.count2);
+            Console.WriteLine(RightTriangle.count3);
+            new RightTriangle(1);
+            new RightTriangle(1);
+            new RightTriangle(1);
+            new RightTriangle(1);
+            new RightTriangle(1);
+            new RightTriangle(1);
+            Console.WriteLine(RightTriangle.count);
+        }
+    }
+}
---
+++ module_2/Seminar_5.11/Task_5/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Task_5
+{
+    internal class VideoFile
+    {
+        private readonly string _name;
+        private readonly int _duration;
+        private readonly int _quality;
+
+        public VideoFile(string name, int duration, int quality)
+        {
+            _name = name;
+            _duration = duration;
+            _quality = quality;
+        }
+
+        public int Size => _duration * _quality;
+
+        public override string ToString()
+        {
+            return $"Name - {_name}\n" +
+                   $"Duration - {_duration} sec. | Quality - {_quality}\n" +
+                   $"Size - {Size}";
+        }
+    }
+    
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите название видеофайла, продолжительность и качество в одну строку, через пробел");
+            Console.WriteLine("///ПРИМЕР ВВОДА: FileName 90 1080");
+            var input = Console.ReadLine().Split(" ");
+            
+            var userVideoFile = new VideoFile(input[0], int.Parse(input[1]), int.Parse(input[2]));
+            var rnd = new Random();
+            
+            var N = rnd.Next(5, 15 + 1);
+            Console.WriteLine($"Размер массива видеофайлов == {N}");
+            var arrayOfVideos = new VideoFile[N];
+
+            for (var i = 0; i < arrayOfVideos.Length; i++)
+            {
+                arrayOfVideos[i] = new VideoFile(RandomFileName(rnd.Next(1, 9 + 1)), rnd.Next(60, 360 + 1),
+                    rnd.Next(100, 1000 + 1));
+            }
+
+            var count = 0;
+            foreach (var video in arrayOfVideos)
+            {
+                if (video.Size > userVideoFile.Size)
+                {
+                    Console.WriteLine(video);
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("Нет файлов разамеров больше данного :( ");
+            }
+            
+            Escape();
+        }
+
+        private static string RandomFileName(int length)
+        {
+            const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var symbols = new char[length];
+            var result = "";
+            var rnd = new Random();
+            foreach (var i in symbols)
+            {
+                symbols[i] = alphabet[rnd.Next(0, alphabet.Length)];
+                result += symbols[i];
+            }
+
+            return result;
+        }
+
+        private static void Escape()
+        {
+            Console.WriteLine("\n\tЖелаете завершить программу?\n");
+            Console.WriteLine("Нажмите [ENTER] чтобы завершить программу...");
+            Console.WriteLine("Нажмите [SPACE] чтобы продолжить...\n");
+            // Чтение клавиши с проверкой.
+            var userChoice = Console.ReadKey();
+            while (userChoice.Key != ConsoleKey.Spacebar && userChoice.Key != ConsoleKey.Enter)
+            {
+                userChoice = Console.ReadKey();
+            }
+            
+            if (userChoice.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("(^ - ^) Пока! (^ - ^)\n");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Main();
+            }
+        }
+    }
+}
