f46b15ce975b8e484d43bca49debc618a2b182c7
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2021-12-17 18:17:02 +0300

    hw 9

---
+++ module_2/Seminar_25.11/Task 2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seminar_25._11
+{
+    public partial class Form1 : Form
+    {
+        private bool IsDisappeared;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IsDisappeared = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsDisappeared == false)
+            {
+                FormDisappearing();
+            }
+            else
+            {
+                FormAppearing();
+            }
+        }
+
+        private void FormDisappearing()
+        {
+            if (label1.Text.Length != 0)
+            {
+                FadeAwayOneCharacter();
+            }
+            else if (Opacity > 0)
+            {
+                Opacity -= 0.2;
+            }
+            else
+            {
+                IsDisappeared = true;
+                label1.Text = "Кот уже ушел!";
+            }
+        }
+
+        private void FadeAwayOneCharacter()
+        {
+            StringBuilder line = new("");
+            for (int i = 0; i < label1.Text.Length - 1; i++)
+            {
+                line.Append(label1.Text[i]);
+            }
+
+            label1.Text = line.ToString();
+        }
+
+        private void FormAppearing()
+        {
+            if (Opacity < 1)
+            {
+                Opacity += 0.2;
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+        }
+    }
+}
---
+++ module_2/Seminar_25.11/Task 4/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_4
+{
+    public partial class Form1 : Form
+    {
+        private static readonly List<string> Items = new List<string>
+        {
+            "один", "два", "три", "четыре", "пять", "шесть", "семь"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var item in Items)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Список пуст или\nнет выбранного элемента","", MessageBoxButtons.OK);
+                return;
+            }
+
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+    }
+}
